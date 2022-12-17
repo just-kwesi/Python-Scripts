195745a06744459b9c604a13b3e514f422cbec0c
Frederick Tetteh <kwesiossom@gmail.com>
2022-12-16 21:23:24 -0500

    empire movies web scrap

---
+++ web_scraping/movies_scrapper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+site_data = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+
+soup = BeautifulSoup(site_data.text,"lxml")
+
+movies = soup.find_all(name="h3",class_="title")
+movies_list = [movie.getText() for movie in movies]
+
+with open("movies.txt", "w") as movies_file:
+    for index in range(99,-1,-1):
+        movie = movies_list[index]
+        movies_file.write(f"{movie}\n")
