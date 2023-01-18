6660f1ea4c2e7c21db82e353914b9d575bcdedb0
Frederick Tetteh <kwesiossom@gmail.com>
2023-01-17 20:54:27 -0500

    hackerank test

---
+++ test.py
@@ -0,0 +1,21 @@
+
+num = 181
+orig = list(str(num))
+se = set(orig)
+
+
+def unique(l, r):
+    count = 0
+    for num in range(l, r+1):
+        orig = list(str(num))
+        s = set(orig)
+        if (len(orig) == len(s)):
+            count += 1
+
+    return count
+
+
+res = []
+res.
+
+print(unique(80, 120))
