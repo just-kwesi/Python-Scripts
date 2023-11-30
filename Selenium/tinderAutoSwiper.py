ce67cd909afb5e4bc66870f9da0244df7a36833e
Frederick Tetteh <kwesiossom@gmail.com>
2023-11-30 17:34:08 -0600

    tinder swiper: done but with default code, used google login

--- Selenium/tinderAutoSwiper.py
+++ Selenium/tinderAutoSwiper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 import time
 import os
 
@@ -21,3 +22,70 @@ time.sleep(3)
 
 sign_in_button = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a")
 sign_in_button.click()
+time.sleep(3)
+
+# sign_in_with_google = driver.find_element(By.XPATH,'/html/body/div/div/div[2]')
+
+# sign_in_with_google = driver.find_element(By.ID,'button-label')
+# sign_in_with_google.click()
+fb_login = driver.find_element(By.XPATH,'/html/body/div/div/div[2]')
+fb_login.click()
+
+
+
+#Switch to Facebook login window
+time.sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+#Login and hit enter
+email = driver.find_element_by_xpath('//*[@id="email"]')
+password = driver.find_element_by_xpath('//*[@id="pass"]')
+email.send_keys(FB_EMAIL)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+#Switch back to Tinder window
+driver.switch_to.window(base_window)
+print(driver.title)
+
+#Delay by 5 seconds to allow page to load.
+time.sleep(5)
+
+#Allow location
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+
+#Disallow notifications
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+
+#Allow cookies
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+
+#Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
+for n in range(100):
+
+    #Add a 1 second delay between likes.
+    time.sleep(1)
+
+    try:
+        print("called")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+
+    #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+
+        #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
+        except NoSuchElementException:
+            time.sleep(2)
+
+driver.quit()
