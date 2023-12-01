fd15c49c1a0aafc16ca5ab6700f1251a23801eec
Frederick Tetteh <kwesiossom@gmail.com>
2023-12-01 17:59:56 -0600

    twitter bot: getting the test started

--- Selenium/x-bot/main.py
+++ Selenium/x-bot/main.py
@@ -2,6 +2,8 @@ from time import sleep
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 import os
 
@@ -22,21 +24,30 @@ chrome_options.add_experimental_option('detach',True)
 
 
 class InternetSpeedTwitterBot:
-    def __init__(self,_down,_up):
+    def __init__(self,driver_service,driver_options):
         self.driver = webdriver.Chrome(service=chrome_service,options=chrome_options)
-        self.down = _down
-        self.up = _up
+        self.down = 0
+        self.up = 0
 
     def get_internet_speed(self):
-        self.driver.get("https://twitter.com/home")
-        sleep(2)
+        self.driver.get("https://www.speedtest.net/")
+        sleep(1)
+
+        # start_button = self.driver.find_element(By.CSS_SELECTOR,".start-button a")
+
+        start_button = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        start_button.click()
+
+        sleep(20)
+
+
 
     def tweet_at_provider(self):
         print('tweeting')
 
 
 
-bot = InternetSpeedTwitterBot("up","down")
+bot = InternetSpeedTwitterBot(chrome_service,chrome_options)
 bot.get_internet_speed()
 bot.tweet_at_provider()
 
