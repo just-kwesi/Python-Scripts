3037e58db020f949577db9f1c89113c7a172ff54
Frederick Tetteh <kwesiossom@gmail.com>
2023-12-04 00:25:09 -0600

    zillow capstone: cleaned up code

--- Selenium/capstone-zillow/main.py
+++ Selenium/capstone-zillow/main.py
@@ -7,7 +7,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 #load environment variables
